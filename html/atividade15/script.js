3b16a507266fbcea392b066a9d0f7091d780ea65
renata <renataa.rcs@gamil.com>
2023-09-01 11:44:13 -0300

    add ativ. 15

---
+++ html/atividade15/script.js
@@ -0,0 +1,19 @@
+
+const titulo = document.getElementsByTagName('h1')
+const divis = document.getElementsByClassName('quadrado')
+const redonda = document.getElementsByClassName('circulo')
+
+//console.log(titulo)
+//console.log(divis)
+//console.log(redonda)
+
+
+// querySelector = consultar ele captura o primeiro item que ele achar no html.
+// qureySelectorAll = consultar ele captura o todos os item que ele achar no html.
+
+const lista = document.querySelectorAll('li')
+
+//console.log(lista)
+// for(i = 0; lista.length; i++){
+//   console.log(lista[i])
+//}
