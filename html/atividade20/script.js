c8c4f95e616c55b17009665bc17ffd51b24cf701
renata <renataa.rcs@gamil.com>
2023-09-15 11:54:58 -0300

    add js

---
+++ html/atividade20/script.js
@@ -0,0 +1,59 @@
+const btnAdicionar = document.getElementById('adicionar')
+const valorTarefa = document.getElementById('text-input') 
+
+btnAdicionar.addEventListener('click', (evento)=>{
+    evento.preventDefault();
+
+    if(valorTarefa.value == ''){
+        alert('Informe uma tarefa valida')
+    }
+    else{
+        const tarefa = criarTarefa(valorTarefa.value)
+        renderizarTarefa(tarefa)
+    }
+
+   
+
+    valorTarefa.value = ''
+
+   
+})
+const listaTarefas = document.querySelector('ul')
+
+listaTarefas.addEventListener('click', (elemento) =>{
+
+    const itemClicado = elemento.target
+
+    if(itemClicado.classList.contains('excluir')){
+        itemClicado.parentElement.parentElement.remove()
+    }
+    if(itemClicado.classList.contains('concluir')){
+        itemClicado.parentElement.parentElement.firstChild.classList.toggle('concluido')
+    }
+
+   
+
+})
+
+function criarTarefa(valorTarefa){
+
+    const li = document.createElement('li')
+
+   
+
+    li.innerHTML = `<p>
+    ${valorTarefa}</p>
+    <div>
+    <button class="excluir"></button>
+    <button class="concluir"></button>
+  </div> `
+
+  return li
+    
+}
+
+function renderizarTarefa(tarefa){
+    const listaTarefas = document.querySelector('ul')
+
+    listaTarefas.appendChild(tarefa)
+}
